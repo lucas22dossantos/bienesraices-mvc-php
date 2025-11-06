77bcc9c5264e73a2e4caaaea2a2b4b1296766214
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-11-06 15:03:36 -0300

    feat(router): registrar URLs y vincular métodos del controlador
    - Se implementa registro de rutas GET.
    - Se asocian las rutas al PropiedadController.
    - Se crea método para renderizar la vista correspondiente.

--- Router.php
+++ Router.php
@@ -4,8 +4,33 @@ namespace MVC;
 
 class Router
 {
-    public function __construct()
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn)
     {
-        echo "Router funcionando...";
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) {
+            // la URL existe y hay una función asociada
+            call_user_func($fn, $this);
+        } else {
+            echo "Página no encontrada";
+        }
+    }
+
+    // mostrar una vista
+    public function render($view)
+    {
+        require __DIR__ . '/views/' . $view . '.php';
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,5 +3,16 @@
 require_once __DIR__ . "/../includes/app.php";
 
 use MVC\Router;
+use Controllers\PropiedadController;
 
 $router = new Router();
+
+
+$router->get('/admin', [PropiedadController::class, 'index']);
+
+$router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
+$router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
+
+
+
+$router->comprobarRutas();
